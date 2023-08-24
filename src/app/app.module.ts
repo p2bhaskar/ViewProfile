4ed9a3679ffd37232c5c159102f4cee24119a5b4
p2bhaskar <86559893+p2bhaskar@users.noreply.github.com>
2023-08-24 20:42:47 +0530

    header changes implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './static/header/header.component';
-
+import {MatToolbarModule } from '@angular/material/toolbar'
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,6 +12,7 @@ import { HeaderComponent } from './static/header/header.component';
   ],
   imports: [
     BrowserModule,
+    MatToolbarModule,
     AppRoutingModule
   ],
   providers: [],
